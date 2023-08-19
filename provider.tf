f6d0c604bb4fb33199fe1a0b8ddbbe48ea933d55
N1khil-c <139035048+N1khil-c@users.noreply.github.com>
2023-08-19 23:38:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket_iam_member" "role" {
-  bucket = "asia-south2-composer-enviro-719fa6a7-bucket"
+  bucket = "db-cicd-wave3"
   role   = "roles/storage.admin"  # Replace with the desired role
 
   member = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
