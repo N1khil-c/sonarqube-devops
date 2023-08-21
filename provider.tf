3920f3e9966d01c68054c5c8eb199c8e9f6a76a2
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-08-22 00:49:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,15 +3,21 @@ provider "google" {
   region  = "asia-south2" 
 }
 
-variable "roles" {
-  type = list(string)
-  default = ["roles/storage.objectCreator", "roles/storage.objects.list"]
-}
+#variable "roles" {
+ # type = list(string)
+  #default = ["roles/storage.objectCreator", "roles/storage.objects.list"]
+#}
 
-resource "google_storage_bucket_iam_binding" "binding" {
-  for_each = toset(var.roles)
-  bucket = "db-cicd-wave3"
+#resource "google_storage_bucket_iam_binding" "binding" {
+ # for_each = toset(var.roles)
+  #bucket = "db-cicd-wave3"
   # role   = "roles/storage.admin"  
-  role     = each.value
-  members = ["serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"]
+ # role     = each.value
+#  members = ["serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"]
+#}
+
+resource "google_storage_bucket_iam_member" "list_permission" {
+  bucket = "db-cicd-wave3"
+  role   = "roles/storage.objects.list"
+  member = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
 }
