66ba5a3c891506c23800918cad195b40474e4ffc
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-08-22 00:20:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = "db-cicdpipeline-wave3"
-  region  = "asia-south2"  # Use appropriate region
+  region  = "asia-south2" 
 }
 
 resource "google_storage_bucket_iam_binding" "binding" {
   bucket = "db-cicd-wave3"
-  # role   = "roles/storage.admin"   Replace with the desired role
+  # role   = "roles/storage.admin"  
   role   = "roles/storage.objects.create"
   members = ["serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"]
 }
