5ef9ed556c701b389dc133afc850a47954c76d68
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-08-21 10:35:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_storage_bucket_iam_member" "role" {
   bucket = "db-cicd-wave3"
-  role   = "roles/storage.admin"  # Replace with the desired role
+  # role   = "roles/storage.admin"   Replace with the desired role
   role   = "roles/storage.objects.create"
   member = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
 }
