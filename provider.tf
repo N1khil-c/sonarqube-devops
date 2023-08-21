0bc597c36d46c3d5f7012404eb5037cfd0f650c6
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-08-22 00:42:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,15 @@ provider "google" {
   region  = "asia-south2" 
 }
 
+variable "roles" {
+  type = list(string)
+  default = ["roles/storage.objectCreator", "roles/storage.objects.list"]
+}
+
 resource "google_storage_bucket_iam_binding" "binding" {
+  for_each = toset(var.roles)
   bucket = "db-cicd-wave3"
   # role   = "roles/storage.admin"  
-  role   = "roles/storage.objectCreator"
-  role = "roles/storage.objects.list"
+  role     = each.value
   members = ["serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"]
 }
