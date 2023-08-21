7b55dbf8d262105a7addd1d2d685e1a72c3cdd15
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-08-21 23:32:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,5 +7,5 @@ resource "google_storage_bucket_iam_binding" "binding" {
   bucket = "db-cicd-wave3"
   # role   = "roles/storage.admin"   Replace with the desired role
   role   = "roles/storage.objects.create"
-  members = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
+  members = ["serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"]
 }
