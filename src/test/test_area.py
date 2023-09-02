122fcc11daae00f6abd9a7e956eb17908b286e7a
N1khil-c <139035048+N1khil-c@users.noreply.github.com>
2023-09-02 13:06:17 +0000

    Update test_area.py

--- src/test/test_area.py
+++ src/test/test_area.py
@@ -4,18 +4,20 @@ import sys
 
 sys.path.insert(0, '/src/main/')
 
-def test_calculate_area_square():
-    assert calculate_area_square(2) == 4
+def test_positive_integer():
+    assert calculate_area_square(5) == 25
+
+def test_positive_float():
     assert calculate_area_square(2.5) == 6.25
 
-def test_calculate_area_square_negative():
-    with pytest.raises(TypeError):
-        calculate_area_square(-2)
+def test_negative_number():
+    with pytest.raises(ValueError):
+        calculate_area_square(-5)
 
-def test_calculate_area_square_string():
+def test_string_input():
     with pytest.raises(TypeError):
-        calculate_area_square("2")
+        calculate_area_square("5")
 
-def test_calculate_area_square_string():
+def test_boolean_input():
     with pytest.raises(TypeError):
-        calculate_area_square("2")
+        calculate_area_square(True)
