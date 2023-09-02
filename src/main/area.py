ab60f1f49aeedc14b2d95b907772ae8159755a08
N1khil-c <139035048+N1khil-c@users.noreply.github.com>
2023-09-02 13:03:15 +0000

    Update area.py

--- src/main/area.py
+++ src/main/area.py
@@ -1,9 +1,39 @@
 def calculate_area_square(length: int | float) -> int | float:
     """
-    Function to calculate the area of a square
-    :param length: length of the square
-    :return: area of the square
+    Function to calculate the area of a square.
+    
+    :param length: The length of the square.
+    :type length: int or float
+    :raises TypeError: If length is not a positive non-zero number.
+    :return: The area of the square.
+    :rtype: int or float
     """
-    if not isinstance(length, (int, float)) or length <= 0:
-        raise TypeError("Length must be a positive non-zero number")
+    if not isinstance(length, (int, float)):
+        raise TypeError("Length must be a number")
+    if length <= 0:
+        raise ValueError("Length must be a positive non-zero number")
+    
+    # Calculate and return the area of the square
     return length * length
+
+# Test cases
+try:
+    # Valid input
+    area = calculate_area_square(5)
+    print(f"Area of the square: {area}")
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    # Invalid input (string)
+    area = calculate_area_square("5")
+    print(f"Area of the square: {area}")
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    # Invalid input (negative number)
+    area = calculate_area_square(-5)
+    print(f"Area of the square: {area}")
+except Exception as e:
+    print(f"Error: {e}")
