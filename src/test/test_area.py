bd15b27eb7ff59fc4e5daefda7cecd67a9e9d9a2
N1khil-c <139035048+N1khil-c@users.noreply.github.com>
2023-09-02 12:26:02 +0000

    Update test_area.py

--- src/test/test_area.py
+++ src/test/test_area.py
@@ -19,16 +19,3 @@ def test_calculate_area_square_string():
 def test_calculate_area_square_string():
     with pytest.raises(TypeError):
         calculate_area_square("2")
-        
-def test_calculate_area_square_list():
-    with pytest.raises(TypeError):
-        calculate_area_square([2])
-
-def test_calculate_area_square_complex_number():
-    with pytest.raises(TypeError):
-        calculate_area_square(2 + 3j)
-
-def test_import_area_circle_vulnerability():
-    # Intentionally creating a vulnerability by providing an incorrect import path
-    with pytest.raises(ImportError):
-        from src.main.invalid_module import calculate_area_circle
